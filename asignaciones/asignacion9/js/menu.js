1d8f0c660bf2455495266bfc7a0d0711aa0fcbef
Dayana998 <dayis12456@gmail.com>
2019-08-29 13:45:31 -0400

    ratita

--- asignaciones/asignacion9/js/menu.js
+++ asignaciones/asignacion9/js/menu.js
@@ -1,16 +1,16 @@
 function MenuDesplegable() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-
-function slide(encabezado) {
+/* */
+function slide() {
     var n = 0;
     var vimg = [];
-    vimg[0] = '<img src="imagenes/villa-tunari.jpg" width: 100%; height:100%;>'
-    vimg[1] = '<img src="imagenes/islaluna.jpg" width: 100%; height:100%;>'
-    vimg[2] = '<img src="imagenes/machia.jpg" width: 100%; height:100%;>'
-    vimg[3] = '<img src="imagenes/titicachi.jpg" width: 100%; height:100%;>'
-    vimg[4] = '<img src="imagenes/copac.jpg" width: 100%; height:100%;>'
-    vimg[5] = '<img src="imagenes/parque.jpg" width: 100%; height:100%;>'
+    vimg[0] = '<img src="imagenes/villa-tunari.jpg" width: 50%;>'
+    vimg[1] = '<img src="imagenes/islaluna.jpg"width: 50%;>'
+    vimg[2] = '<img src="imagenes/machia.jpg"width: 50%;>'
+    vimg[3] = '<img src="imagenes/bahia.jpg"width: 50%;>'
+    vimg[4] = '<img src="imagenes/carrasco.jpg"width: 50%;>'
+    vimg[5] = '<img src="imagenes/parque.jpg"width: 50%;>'
     
     var m = document.getElementById('slide')
     window.onload = window.setInterval(function() {
@@ -20,5 +20,5 @@ function slide(encabezado) {
         } else {
             n = 0;
         }
-    }, 1000)
+    },2000)
 }
