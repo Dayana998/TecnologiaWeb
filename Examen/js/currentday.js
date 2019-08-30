440c0e1b5b2d859401112caa21bf5a967c297d3e
Dayana998 <dayis12456@gmail.com>
2019-08-30 13:17:52 -0400

    kurfha,br.guah

--- Examen/js/currentday.js
+++ Examen/js/currentday.js
@@ -1,1 +1,6 @@
 ﻿// fecha actual dinámicamente en el siguiente formato de fecha: lunes 6 de abril de 2020
+function fecha(){
+    var f = new Date();
+                    document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+    }
+    fecha();
